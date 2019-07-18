f12be228168690baf81c5a307b6093657c50f0dc
icepants <shawnluther99@gmail.com>
2019-07-17 22:26:00 -0400

    Performed a code cleanup on the run file and js file. I also fixed a bug causing the 'item not in stock' handler to produce negitive values on items already at '0'.

--- assets/js/Run Bamazon Cli.bat
+++ assets/js/Run Bamazon Cli.bat
@@ -1,8 +1,14 @@
-:: Quick Launcher For Bamazon CLI.
+:: Quick Launcher For Bamazon CLI (Highly Recommended).
 
+:: Run Node App.
 :QuickLaunchBamazon
+:: Shut off dir paths.
 @echo off
+:: Tell the user to be patient, the node app is executing.
 echo Loading, Please Wait....
+:: Run node app in CLI.
 node bamazonCustomer
+:: Press any key to continue.
 pause
+:: If bamazon finishes an execution with no end handle, loop back to table view.
 goto QuickLaunchBamazon
--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -20,7 +20,7 @@ function productTable() {
     console.clear();
     // Use our SQL products table.
     connection.query("SELECT * FROM products", function (err, res) {
-        // Catch any errors.
+        // If there is an error, handle it.
         if (err) throw err;
 
         // Create our new table.
@@ -71,14 +71,24 @@ function productTable() {
                 connection.query("SELECT * FROM products WHERE item_id = " + product.item_id, function (err, res) {
                     // If there is an error, handle it.
                     if (err) throw err;
+
                     // Default Amount When No Data Is Populated.
                     var totalCost = 0;
+
                     // Calculate price based on total price * total units sold during transaction. 
                     totalCost = res[0].price * product.stock_quantity;
 
+                    // If there isn't enough product units, let the customer know.
                     if (res[0].stock_quantity <= 0) {
-                        console.log("Insufficient quantity!");
+                        console.log("\n! Insufficient Quantity: This item is sold out, sorry for the inconvenience!");
+
+                        // Add back the deduction to avoid negitive values.
+                        connection.query("UPDATE products SET stock_quantity = stock_quantity +" + product.stock_quantity + " WHERE item_id =" + product.item_id, function (err, res) {
+                            // If there is an error, handle it.
+                            if (err) throw err;
+                        });
                     } else {
+
                         // When the customer is finished shopping, show their virtual receipt.
                         console.clear();
                         console.log(`
@@ -98,18 +108,21 @@ function productTable() {
 
                                                 Inventory Updated!
                                                   Total purchase:\n
-                                                    `+ "$" + totalCost + `
+                                                     `+ "$" + totalCost + `
                     -------------------------------------------------------------------------------
                             
-                                                `);
+                    `);
                     }
                 });
+
                 // Deduct however many purchases the customer wants to make from the selected row.
                 connection.query("UPDATE products SET stock_quantity = stock_quantity -" + product.stock_quantity + " WHERE item_id =" + product.item_id, function (err, res) {
+                    // If there is an error, handle it.
                     if (err) throw err;
+
+                    // Close connection.
+                    connection.end();
                 });
-                // Close connection.
-                connection.end();
             });
 
     });
