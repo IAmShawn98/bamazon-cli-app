de92a4d5d4e23eac88d2977a33d3ebff254e9065
icepants <shawnluther99@gmail.com>
2019-07-18 00:56:20 -0400

    Rewrote the Bamazon launcher to be a bit more diverse for future content.

--- assets/js/Run Bamazon Cli.bat
+++ assets/js/Run Bamazon Cli.bat
@@ -1,14 +1,43 @@
-:: Quick Launcher For Bamazon CLI (Highly Recommended).
-
-:: Run Node App.
-:QuickLaunchBamazon
-:: Shut off dir paths.
+:: Quick Launcher For Bamazon CLI App (Highly Recommended).
 @echo off
-:: Tell the user to be patient, the node app is executing.
-echo Loading, Please Wait....
-:: Run node app in CLI.
-node bamazonCustomer
-:: Press any key to continue.
+
+:: Let the user pick which node script they wish to run.
+:selectMenu
+cls
+echo.
+echo  Welcome to Bamazon CLI, the command line storefront! What role do you assume? Let us know.
+echo.
+echo  1.) I'm A Customer.
+echo  2.) I'm A Manager.
+echo  3.) I'm the Supervisor.
+echo.
+echo  Type 1, 2, or 3 and press 'enter'.
+echo.
+
+:: If the user selects 1, 2, or 3 go to the proper node views.
+set /p role=:
+if %role% == 1 goto customerView
+if %role% == 2 goto managerView
+if %role% == 3 goto supervisorView
+
+:: Launch Customer View.
+:customerView
+cls
+echo Loading View.... Please Wait.
+node bamazonCustomer.js
 pause
-:: If bamazon finishes an execution with no end handle, loop back to table view.
-goto QuickLaunchBamazon
+goto customerView
+
+:: Launch Manager View.
+:managerView
+cls
+echo This portion of Bamazon is not yet implemented. Try again later.
+pause
+goto selectMenu
+
+:: Launch Supervisor View.
+:supervisorView
+cls
+echo This portion of Bamazon is not yet implemented. Try again later.
+pause
+goto selectMenu
