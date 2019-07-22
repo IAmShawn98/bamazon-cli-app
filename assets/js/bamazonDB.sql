41924226d53af2465fffbcfd30ec85925f169e9e
icepants <shawnluther99@gmail.com>
2019-07-21 21:12:44 -0400

    Fixed a bug causing prices entered by the user to format without leading zeros / values.

--- assets/js/bamazonDB.sql
+++ assets/js/bamazonDB.sql
@@ -1,5 +1,5 @@
 -- Uncomment and execute line '2' to start fresh if needed! --
--- DROP DATABASE IF EXISTS bamazon; --
+DROP DATABASE IF EXISTS bamazon;
 -- CREATE and USE our Bamazon DATABASE --
 CREATE DATABASE bamazon;
 USE bamazon;
@@ -9,7 +9,7 @@ CREATE TABLE products (
 	item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NULL,
     department_name VARCHAR(100) NULL,
-    price decimal(10,20) NULL,
+    price decimal(10,2) NULL,
     stock_quantity INT NULL,
     PRIMARY KEY (item_id)
 );
